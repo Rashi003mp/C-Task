bd281ea8bd4d468e30ca2d77b05f28a6445a7663
Rashi003mp <muhammedrashidr222@gmail.com>
2025-09-10 18:09:47 +0530

    Add project files.

---
+++ C# Task/BasicOparations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class BasicOperations
+    {
+        public static void CheckOddEven(int number) 
+        {
+          if(number % 2 == 0)
+           Console.WriteLine($"{number} is Even");
+            else
+                Console.WriteLine($"{number} is Odd");
+        }
+        public static void CheckNumberSign(int number)
+        {
+            if (number > 0)
+                Console.WriteLine($"{number} is Positive");
+            else if (number < 0)
+                Console.WriteLine($"{number} is Negative");
+            else
+                Console.WriteLine($"{number} is Zero");
+        } 
+        public static void FindLargest(int a, int b, int c)
+        {
+            int largest;
+            if (a >=b && a >= c)
+                largest = a;
+            else if (b >= a && b >= c)
+                largest = b;
+            else
+                largest = c;
+            
+            Console.WriteLine($"LArgest number is {largest}");
+       }
+        public static void CheckLeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0)|| year %400==0)
+                Console.WriteLine($"{year} is a Leap Year");
+            else
+                Console.WriteLine($"{year} is not a Leap Year");
+        }
+        public static void FindGrade(int grade)
+        {
+            if (grade >= 90 && grade <= 100)
+                Console.WriteLine($"S grade{grade}");
+            else if (grade >= 80 && grade <= 90)
+                Console.WriteLine($"A grade{grade}");
+            else if (grade >= 70 && grade <= 80)
+                Console.WriteLine($"B grade{grade}");
+            else if (grade >= 60 && grade <= 70)
+                Console.WriteLine($"C grade{grade}");
+            else if (grade >= 50 && grade <= 60)
+                Console.WriteLine($"D grade{grade}");
+            else if (grade >= 40 && grade <= 50)
+                Console.WriteLine($"E grade{grade}");
+            else if (grade >= 0 && grade <= 40)
+                Console.WriteLine($"Studen has failed {grade}");
+            else 
+                Console.WriteLine("Invalid grade");
+        }
+
+    }
+}
---
+++ C# Task/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a program to run:");
+            Console.WriteLine("1. Check Odd or Even");
+            Console.WriteLine("2. Check Positive, Negative, or Zero");
+            Console.WriteLine("3. Find Largest of Three Numbers");
+            Console.WriteLine("4. Check Leap Year");
+            Console.WriteLine("5. Find Grade");
+
+            Console.Write("Enter choice (1-5): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter a number: ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    BasicOperations.CheckOddEven(num1);
+                    break;
+                case 2:
+                    Console.Write("Enter a number: ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    BasicOperations.CheckNumberSign(num2);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter three numbers:");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    int c = Convert.ToInt32(Console.ReadLine());
+                    BasicOperations.FindLargest(a, b, c);
+                    break;
+                case 4:
+                    Console.Write("Enter a year: ");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    BasicOperations.CheckLeapYear(year);
+                    break;
+                case 5:
+                    Console.WriteLine("Enter Mark");
+                    int mark = Convert.ToInt32(Console.ReadLine());
+                    BasicOperations.FindGrade(mark);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+
+            }
+        }
+    }
+}
