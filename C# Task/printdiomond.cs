b0139a733dc879c920ad7b573b9806f7bb28c43f
Rashi003mp <muhammedrashidr222@gmail.com>
2025-09-18 16:05:38 +0530

    diamond set

--- C# Task/printdiomond.cs
+++ C# Task/printdiomond.cs
@@ -1,19 +1,41 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace BasicPrograms
-//{
-//    internal class printdiomond
-//    {
-//        static void main(string [] args)
-//        {
+namespace BasicPrograms
+{
+    internal class Printdiomond
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
 
-//        }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                for (int j = 1; j <= (2 * i - 1); j++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                for (int j = 1; j <= (2 * i - 1); j++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+
+        }
 
 
 
-//    }
-//}
+    }
+}
